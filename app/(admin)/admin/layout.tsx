dc15a7d4621200e2866f18de9be60e49c8e07106
samKenpachi011 <samuelkabelo1@gmail.com>
2025-10-17 14:53:47 +0200

    FIX: admin page routes

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -1,30 +1,33 @@
 'use client'
-
-import PatentAdminSidebar from '@/app/utils/nav/patentAdminSidebar'
+import AdminSidebar from '@/app/utils/nav/adminSidebar'
 import Topbar from '@/app/utils/nav/topbar'
 import { useState } from 'react'
-import AdminSidebar from '../utils/nav/adminSidebar'
 
 interface LayoutProps {
     children: React.ReactNode
 }
-const AdminAppLayout: React.FC<LayoutProps> = ({ children }) => {
+const AppLayout: React.FC<LayoutProps> = ({ children }) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false)
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen)
     }
     return (
-  <div
-                // className={`flex flex-col flex-1 transition-all duration-300 ${
-                //     isSidebarOpen ? 'ml-64' : 'ml-0'
-                // }`}
+        <div className="flex-1 flex min-h-screen bg-background">
+            {/* Sidebar */}
+            <AdminSidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+
+            {/* Main content area */}
+            <div
+                className={`flex flex-col flex-1 transition-all duration-300 ${
+                    isSidebarOpen ? 'ml-64' : 'ml-0'
+                }`}
             >
                 {/* Topbar */}
-                {/* <Topbar
+                <Topbar
                     username="Testuser"
                     accountHostname="localhost"
                     toggleSidebar={toggleSidebar}
-                /> */}
+                />
 
                 {/* Main content */}
                 <main className="flex-1 overflow-y-auto p-4 ">
@@ -36,7 +39,8 @@ const AdminAppLayout: React.FC<LayoutProps> = ({ children }) => {
                     © {new Date().getFullYear()} KitsoHub | Version 1.0.0
                 </footer>
             </div>
+        </div>
     )
 }
 
-export default AdminAppLayout
+export default AppLayout
---
+++ app/(admin)/patents/layout.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { AppSidebar } from '@/app/components/community/app-sidebar'
+import CommunityBase from '@/app/components/community/communityBase'
+import { PatentsAppSidebar } from '@/app/components/patents/patents-app-sidebar'
+import PatentsBase from '@/app/components/patents/patentsBase'
+import { SidebarProvider } from '@/app/components/ui/sidebar'
+import PatentAdminSidebar from '@/app/utils/nav/patentAdminSidebar'
+import Topbar from '@/app/utils/nav/topbar'
+import { ThemeProvider } from 'next-themes'
+import { useState } from 'react'
+
+interface LayoutProps {
+    children: React.ReactNode
+}
+const AdminAppLayout: React.FC<LayoutProps> = ({ children }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen)
+    }
+    return (
+
+
+                <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange >
+                    <SidebarProvider>
+
+                        <div className="flex-1 flex min-h-screen">
+                            <PatentsAppSidebar />
+                            <div className="flex-1 flex flex-col bg-card ">
+                                <PatentsBase>
+
+                                    <main className="flex-1 h-full overflow-y-auto p-4 ">{children}</main>
+
+                                </PatentsBase>
+                            </div>
+                        </div>
+                    </SidebarProvider>
+                </ThemeProvider>
+    )
+}
+
+export default AdminAppLayout
